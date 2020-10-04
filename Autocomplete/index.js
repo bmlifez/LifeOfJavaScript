266af7fedf271e7c9a4659593527492d70e835b0
bhaskar.mishra <bhaskar.mishra@infoedge.com>
2020-10-04 11:07:24 +0530

    product-add-remove-status

--- Autocomplete/index.js
+++ Autocomplete/index.js
@@ -1,28 +1,32 @@
 const search = document.getElementById('search');
 const matchList = document.getElementById('matchList');
+const productView = document.getElementById('productView');
 
-const debounce = (fn,delay) => {
+let matches = [];
+let productComparison = [];
+
+const debounce = (fn, delay) => {
     try {
         let timer = null;
-        return function(){
+        return function () {
             clearTimeout(timer);
-            timer = setTimeout(function(){
+            timer = setTimeout(function () {
                 fn.apply(this, arguments);
-            },delay)
-        } 
+            }, delay)
+        }
     } catch (error) {
         console.error(`debounce function ${fn} failed ${error}`);
     }
 }
 
-search.addEventListener('input',debounce(function(){
+search.addEventListener('input', debounce(function () {
     searchList(search.value);
-},2000))
+}, 2000))
 
 const searchList = async (searchText) => {
     const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false');
     const state = await response.json();
-    let matches = state.filter(state => {
+    matches = state.filter(state => {
         const regex = new RegExp(`^${searchText}`, 'gi')
         return state.name.match(regex);
     })
@@ -37,14 +41,58 @@ const searchList = async (searchText) => {
 }
 
 const outputHTML = (matches) => {
-    const html = matches.map(match => {
+    const html = matches.map(coin => {
         return (
-            `<li class="suggestion-li">
-                <span class="span-li">${match.name}</span>
-                <img class="img-icon span-li" src=${match.image} />
-                <span>${match.current_price}</span>
+            `<li  class="suggestion-li" onClick=updateCart("${coin.id}","add")>
+                <span class="span-li">${coin.name}</span>
+                <img class="img-icon span-li" src=${coin.image} />
+                <span>${coin.current_price}</span>
             </li>`
         )
     })
     matchList.innerHTML = html;
+}
+
+const uniqueArray = (inp) => {
+    inp = [...new Set(inp)];
+}
+
+const updateCart = (crypto, type) => {
+    
+    if (type === "add") {
+        for (let i = 0; i < matches.length; i++) {
+            if (matches[i].id === crypto) {
+                productComparison.push(matches[i]);
+            }
+        }
+    }
+
+    if (type === "delete") {
+        let index = 0;
+        for (let i = 0; i < productComparison.length; i++) {
+            if (productComparison[i].id === crypto) {
+                index = i;
+                console.log("match");
+            }
+        }
+        productComparison.splice(index, 1);
+        productComparison;
+    }
+    uniqueArray(productComparison);
+    let html = productComparison.map((ele) => {
+        return (
+            `<div class="flex-container">
+                <span class="span-li">${ele.name}</span>
+                <img class="img-icon span-li" src=${ele.image} />
+                <div>
+                    <span>${ele.current_price}</span>
+                </div>
+                <button onClick=updateCart("${ele.id}","delete")>Remove</button>
+            </div>`
+        )
+    })
+
+    productView.innerHTML = html;
+    matches = [];
+    outputHTML(matches);
 }
